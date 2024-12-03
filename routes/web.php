b104d6933af422e8cafac9fcb5554b70afee3f88
Melake Sisay <melakesisay@gmail.com>
2024-12-03 14:22:36 +0300

    Adding A routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FileExplorerController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/explorer', [FileExplorerController::class, 'index'])->name('explorer.index');
+Route::get('/explorer/navigate', [FileExplorerController::class, 'navigate'])->name('explorer.navigate');
